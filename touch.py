ea24fbcb173d5b35f5f72e86d746abfdb532dc9c
David Higgins <sligodave@gmail.com>
2014-02-16 21:44:23 -0700

    Moved the edit view util from touch into the base of Utils

--- touch.py
+++ touch.py
@@ -12,7 +12,6 @@ Thoughts:
 import time
 
 import sublime_plugin
-import sublime
 
 
 TOUCH_EVENT_TIME = None
@@ -64,24 +63,3 @@ class LiveEventListener(sublime_plugin.EventListener):
     def on_close(self, view):
         remove_event_handlers(view)
         remove_event_handlers_async(view)
-
-
-###################
-# UTILITIES
-###################
-
-
-class TouchEditViewContentCommand(sublime_plugin.TextCommand):
-    def run(self, edit, data=None, start=0, end=None):
-        was_read_only = self.view.is_read_only()
-        if was_read_only:
-            self.view.set_read_only(False)
-        if end is not None and not start == end:
-            if data is not None:
-                self.view.replace(edit, sublime.Region(start, end), data)
-            else:
-                self.view.erase(edit, sublime.Region(start, end))
-        elif data is not None:
-            self.view.insert(edit, start, data)
-        if was_read_only:
-            self.view.set_read_only(True)
