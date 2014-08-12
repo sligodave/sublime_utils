170c074c5e7f70d22b598d541be5a6d5a2fbe98b
David Higgins <sligodave@gmail.com>
2014-08-11 18:54:25 -0600

    Renamed commands.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,22 @@
 
-__version__ = "0.0.2"
+import sublime
+import sublime_plugin
+
+
+class UtilsEditViewCommand(sublime_plugin.TextCommand):
+    def run(self, edit, data=None, start=0, end=None):
+        start = int(start)
+        if end is not None:
+            end = int(end)
+        was_read_only = self.view.is_read_only()
+        if was_read_only:
+            self.view.set_read_only(False)
+        if end is not None and not start == end:
+            if data is not None:
+                self.view.replace(edit, sublime.Region(start, end), data)
+            else:
+                self.view.erase(edit, sublime.Region(start, end))
+        elif data is not None:
+            self.view.insert(edit, start, data)
+        if was_read_only:
+            self.view.set_read_only(True)
